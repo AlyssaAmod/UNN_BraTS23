0c22b2277b7d5f9518d6af41c86a59d1e30684e5
alexandrasmith <alexandrasmith0904@gmail.com>
2023-06-27 16:00:34 +0200

    transform updates

--- scripts/data_transforms.py
+++ scripts/data_transforms.py
@@ -1,4 +1,5 @@
 from torchvision import transforms
+import albumentations as A
 import torchio as tio
 import torch
 import warnings
@@ -13,25 +14,19 @@ def define_transforms():
             transforms.RandomHorizontalFlip(p=0.3),
             transforms.RandomVerticalFlip(p=0.3)
         ]),
-        'fakeSSA': transforms.Compose([
-            # one of?
-            # these arent applied with a probability?
+        'fakeSSA': A.OneOf([
             transforms.GaussianBlur(kernel_size=(21, 21), sigma=(0.5, 1.5)),
             tio.transforms.RandomNoise(mean=0, std=(0, 0.33)), # Gaussian noise
             transforms.ColorJitter(brightness=(0.8, 1.2)),
             tio.transforms.RandomMotion(num_transforms=3, image_interpolation='nearest'),
             tio.transforms.RandomBiasField(coefficients=1),
             tio.transforms.RandomGhosting(intensity=1.5)
-        ]),
+        ], p=0.5), # randomly apply ONE of these given transforms with prob 0.5 
         'val': transforms.Compose([
-            # transforms.Resize(INPUT_SIZE),
-            # transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # inception
+            # transforms.Resize(INPUT_SIZE)
         ]),
         'test' : transforms.Compose([
-            # transforms.Resize(INPUT_SIZE),
-            # transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # inception
+            # transforms.Resize(INPUT_SIZE)
         ])
     }
 
