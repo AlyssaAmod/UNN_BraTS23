5d31fbbaa4f0b77d8bed99504c65c775a15e0fe9
AlyssaAmod <87081013+AlyssaAmod@users.noreply.github.com>
2023-07-17 21:21:58 +0200

    repo clean up

--- playground/data_prep-augs/prepoc_augs/Augs.py
+++ playground/data_prep-augs/prepoc_augs/Augs.py
@@ -1,3 +1,19 @@
+# Mixed Structure Regularisation: corrupt the input data by randomly adding different brain images from BraTS training set, as described by \cite{atya_non_2021}. 
+
+# We can represent this data augmentation with the function: \begin{equation*}MSR(x) = (1 - \alpha ) * x + \alpha * {x_r}\tag{5}\end{equation*}. 
+# where x is the original image, and xr is a randomly selected image. 
+# This is applied with probability and magnitude of P=0.5, $\alpha$=1∗10−4.
+
+
+# Shuffle Pixel Noise: A random permutation of the pixels is chosen and then the same permutation is applied to all the images in the training set \cite{atya_non_2021}: 
+
+# We can represent this as \begin{equation*}SPN(x) = (1 - \alpha ) * x + \alpha * {x_r}\tag{6}\end{equation*}, 
+# where x is the original image, and xr is the image after shuffling pixels on the x, y axis. 
+# This is applied with probability and magnitude: P=1, $\alpha$=1∗10−7.
+
+
+
+
 #1. Rotation class
 class Rotate(object):
   def __init__(self,angle):
--- playground/notUsable/prepoc_augs/spn.py
+++
@@ -1,5 +0,0 @@
-# Shuffle Pixel Noise: A random permutation of the pixels is chosen and then the same permutation is applied to all the images in the training set \cite{atya_non_2021}: 
-
-# We can represent this as \begin{equation*}SPN(x) = (1 - \alpha ) * x + \alpha * {x_r}\tag{6}\end{equation*}, 
-# where x is the original image, and xr is the image after shuffling pixels on the x, y axis. 
-# This is applied with probability and magnitude: P=1, $\alpha$=1∗10−7.
