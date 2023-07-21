c0028f3bb103af6b8fcb49eb61181079f38faf12
Number One Hundred Eighty Seven Guest <guest187@gra-login3.graham.sharcnet>
2023-07-21 11:49:55 -0400

    minor changes

--- scripts/monai_trainer.py
+++ scripts/monai_trainer.py
@@ -60,46 +60,6 @@ results_dir = args.results
 """
 Potentially useful functions for model tracking and checkpoint loading
 """
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = np.where(self.count > 0, self.sum / self.count, self.sum)
-
-
-def datafold_read(datalist, basedir, fold=0, key="training"):
-    with open(datalist) as f:
-        json_data = json.load(f)
-
-    json_data = json_data[key]
-
-    for d in json_data:
-        for k in d:
-            if isinstance(d[k], list):
-                d[k] = [os.path.join(basedir, iv) for iv in d[k]]
-            elif isinstance(d[k], str):
-                d[k] = os.path.join(basedir, d[k]) if len(d[k]) > 0 else d[k]
-
-    tr = []
-    val = []
-    for d in json_data:
-        if "fold" in d and d["fold"] == fold:
-            val.append(d)
-        else:
-            tr.append(d)
-
-    return tr, val
-
 
 def save_checkpoint(model, epoch, best_acc=0, dir_add=results_dir, args=args):
     filename=f"chkpt_{args.run_name}_{epoch}_{best_acc}.pt"
@@ -117,10 +77,10 @@ model=UNet(
     spatial_dims=3,
     in_channels=4,
     out_channels=4,
-    channels=(16, 32, 64, 128, 256),
+    # channels=(16, 32, 64, 128, 256),
     # channels=(32, 64, 128, 256, 320, 320), #nnunet channels, deoth 6
-    # channels=(64, 96, 128, 192, 256, 384, 512) # optinet, depth 7
-    strides=(2, 2, 2, 2), # length should = len(channels) - 1
+    channels=(64, 96, 128, 192, 256, 384, 512) # optinet, depth 7
+    strides=(2, 2, 2, 2, 2, 2), # length should = len(channels) - 1
     # kernel_size=,
     # num_res_units=,
     # dropout=0.0,
